fe57eb11de136394ba7c9dfb16c568285869d810
Aidar Chakiev <achakiev2003@gmail.com>
2020-10-11 10:20:56 +0600

    commit

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request
 
 
 
-#import pdfkit
-#pdfkit.from_url('http://127.0.0.1:5000/workhistory', 'cv.pdf')
-
 app = Flask(__name__)
 
 uname = ''
@@ -25,11 +22,24 @@ employer1 = ''
 start_date1 = ''
 end_date1 = ''
 
-work_title2 = ''
-employer2 = ''
-start_date2 = ''
-end_date2 = ''
 
+
+edu1 = ''
+spec1 = ''
+edu_start_date1 = ''
+edu_end_date1 = ''
+
+edu2 = ''
+spec2 = ''
+edu_start_date2 = ''
+edu_end_date2 = ''
+
+
+languages = ''
+lans = []
+
+skills = ''
+s_skill = []
 
 @app.route('/')
 def index():
@@ -50,7 +60,7 @@ def getval():
 
 
 
-@app.route('/about', methods=['POST'])
+@app.route('/about', methods=['POST', 'GET'])
 def getval1():
     global uname
     global surname
@@ -58,11 +68,18 @@ def getval1():
     global tel
     global occupation
     global about
+    global work_title
+    global employer
+    global start_date
+    global end_date
+
     occupation = request.form['occupation']
     about = request.form['about']
     return render_template('about.html',n = uname, s = surname, em = email, tel = tel, occ = occupation, about=about)
 
-@app.route('/workhistory', methods=['POST'])
+
+
+@app.route('/workhistory', methods=['POST', 'GET'])
 def getval2():
     global uname
     global surname
@@ -70,7 +87,138 @@ def getval2():
     global tel
     global occupation
     global about
-    return render_template('cv.html',n = uname, s = surname, em = email, tel = tel, occ = occupation, about=about)
+    global work_title
+    global employer
+    global start_date
+    global end_date
+    global work_title1
+    global employer1
+    global start_date1
+    global end_date1
+
+    work_title = request.form['work']
+    employer = request.form['employer']
+    start_date = request.form['work_start_date']
+
+    if request.form.get('check') != None:
+        end_date = request.form['work_end_date']
+    else:
+        end_date = 'Present'
+        
+
+        
+    return render_template('workhistory.html',n = uname, s = surname, em = email, tel = tel, occ = occupation, about=about,
+    work_t = work_title, emplo = employer, start = start_date, end = end_date)
+
+
+@app.route('/addwork', methods=['POST', 'GET'])
+def addwork():
+    global uname
+    global surname
+    global email
+    global tel
+    global occupation
+    global about
+    global work_title
+    global employer
+    global start_date
+    global end_date
+    global work_title1
+    global employer1
+    global start_date1
+    global end_date1
+
+    work_title1 = request.form['work1']
+    employer1 = request.form['employer1']
+    start_date1 = request.form['work_start_date1']
+
+    if request.form.get('check') != None:
+        end_date1 = request.form['work_end_date1']
+    else:
+        end_date1 = 'Present'
+
+    return render_template('addwork.html', n=uname, s=surname, em=email, tel=tel, occ=occupation, about=about,
+                           work_t=work_title, emplo=employer, start=start_date, end=end_date,
+                           work_t1=work_title1, emplo1=employer1, start1=start_date1, end1=end_date1)
+
+
+
+
+@app.route('/education', methods = ['POST'])
+def getval3():
+    return render_template('education.html')
+
+
+@app.route('/addeducation', methods = ['POST'])
+def addeducation():
+    global edu1
+    global spec1
+    global edu_start_date1
+    global edu_end_date1
+
+    edu1 = request.form['edu1']
+    spec1 = request.form['spec1']
+    edu_start_date1 = request.form['edu_start_date1']
+    edu_end_date1 = request.form['edu_end_date1']
+    return render_template('addeducation.html', edu1 = edu1, spec1 = spec1, edu_start_date1 = edu_end_date1)
+
+
+@app.route('/skills', methods = ['POST'])
+def done():
+
+    global languages
+    global lans
+    global skills
+    global s_skill
+    global edu2
+    global spec2
+    global edu_start_date2
+    global edu_end_date2
+    return render_template('skills.html')
+
+@app.route('/addskills', methods = ['POST'])
+def check():
+    global uname
+    global surname
+    global email
+    global tel
+    global occupation
+    global about
+
+    global work_title
+    global employer
+    global start_date
+    global end_date
+
+    global work_title1
+    global employer1
+    global start_date1
+    global end_date1
+
+    global work_title2
+    global employer2
+    global start_date2
+    global end_date2
+
+    global edu1
+    global spec1
+    global edu_start_date1
+    global edu_end_date1
+    global edu2
+    global spec2
+    global edu_start_date2
+    global edu_end_date2
+    languages = request.form['languages']
+
+
+    skills = request.form['skills']
+
+    return render_template('splash.html', n=uname, s=surname, em=email, tel=tel, occ=occupation, about=about,
+                           work_t=work_title, emplo=employer, start=start_date, end=end_date,
+                           work_t1=work_title1, emplo1=employer1, start1=start_date1, end1=end_date1,
+                           edu1=edu1, spec1=spec1, edu_start_date1=edu_end_date1,
+                           edu2=edu2, spec2=spec2, edu_start_date2=edu_end_date2,
+                           )
 
 
 
